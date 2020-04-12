2e391ba5f6ee3e8ca5b378f911e79dcd655063a2
pln324 <47726343+pln324@users.noreply.github.com>
2020-04-11 20:40:00 -0500

    update main with javaFX stuff

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -1,8 +1,29 @@
 package assignment5;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.fxml.FXMLLoader;
 
-public class Main {
 
-    public static void main(String[] args) {
-        // launch(args);
-    }
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("CritterGUI.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Critter.displayWorld(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
